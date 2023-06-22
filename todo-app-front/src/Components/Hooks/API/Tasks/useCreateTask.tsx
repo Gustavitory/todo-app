dc58b0d275639cd881df9f4f1c3a7bb651e49c45
Gustavo Riera <78607300+Gustavitory@users.noreply.github.com>
2023-06-21 22:43:54 -0400

    setting ls

--- todo-app-front/src/Components/Hooks/API/Tasks/useCreateTask.tsx
+++ todo-app-front/src/Components/Hooks/API/Tasks/useCreateTask.tsx
@@ -5,6 +5,7 @@ import { createTaskAction } from "../../../../Redux/Reducers/Tasks";
 export const useCreateTask = () => {
   const dispatch = useDispatch();
   const createTask = (body: any) => {
+    console.log(body);
     dispatch(createTaskAction(body));
   };
   return { createTask };
--- todo-app-front/src/Components/Organisms/DragDrop/DragDrop.tsx
+++ todo-app-front/src/Components/Organisms/DragDrop/DragDrop.tsx
@@ -1,46 +1,49 @@
-import './DragDrop.css'
-import { ContainerDD } from '../../Atoms/ContainerDD/ContainerDD';
-import { useSelector } from 'react-redux';
-import { useState } from 'react';
+import "./DragDrop.css";
+import { ContainerDD } from "../../Atoms/ContainerDD/ContainerDD";
+import { useSelector } from "react-redux";
+import { useState } from "react";
 
-type status='Pending'|'In progress'|'Success'
+type status = "Pending" | "In progress" | "Success";
 
 export const DragDrop = () => {
-    const [draggin,setDraggin]=useState(false)
-    const handleDraggin=(draggin:boolean)=>setDraggin(draggin);
+  const [draggin, setDraggin] = useState(false);
+  const handleDraggin = (draggin: boolean) => setDraggin(draggin);
 
-    const typesTaks:status[]=['Pending','In progress','Success']
-    const tasks=useSelector((state:any)=>state.tasks)
-    const {pendingTasks,inProgressTasks,successTasks}=tasks;
-    function asignData(status:string){
-        switch (status){
-            case 'Pending':
-                return pendingTasks
-            case 'In progress':
-                return inProgressTasks
-            case 'Success':
-                return successTasks.filter((task:any)=>task.finishDate.split('T')[0]===new Date().toISOString().split('T')[0])
-            default:
-                return []
-        }
+  const typesTaks: status[] = ["Pending", "In progress", "Success"];
+  const tasks = useSelector((state: any) => state.tasks);
+  const { tasksList } = tasks;
+  function asignData(status: string) {
+    switch (status) {
+      case "Pending":
+        return tasksList.filter((task: any) => task.status === "Pending");
+      case "In progress":
+        return tasksList.filter((task: any) => task.status === "In progress");
+      case "Success":
+        return tasksList
+          .filter((task: any) => task.status === "Success")
+          .filter(
+            (task: any) =>
+              task.finishDate.split("T")[0] ===
+              new Date().toISOString().split("T")[0]
+          );
+      default:
+        return [];
     }
+  }
   return (
-    <div className='grid'>
-        {
-            typesTaks.map(cont=>{
-                return (
-                    <ContainerDD
-                        status={cont}
-                        key={cont}
-                        data={asignData(cont)}
-
-                        isDraggin={draggin}
-                        handleDraggin={handleDraggin}
-                        isSuccess={cont==='Success'}
-                    />
-                )
-            })
-        }
+    <div className="grid">
+      {typesTaks.map((cont) => {
+        return (
+          <ContainerDD
+            status={cont}
+            key={cont}
+            data={asignData(cont)}
+            isDraggin={draggin}
+            handleDraggin={handleDraggin}
+            isSuccess={cont === "Success"}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
